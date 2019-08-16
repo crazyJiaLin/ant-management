fb85515a3a95bb024c78cda4824f6b1025ee3aa8
crazyjialin <crazyjialin@foxmail.com>
2019-08-16 09:18:21 +0800

    add button col+htmlType finish form布局

--- src/components/m-form/m-button/m-button.vue
+++ src/components/m-form/m-button/m-button.vue
@@ -1,17 +1,21 @@
 <template>
-  <a-button :disabled="options.attribute.disabled" :loading="options.attribute.loading"
-            :size="options.attribute.size" :type="options.attribute.type"
-            :icon="options.attribute.icon" @click="onClick">
-    {{options.text}}
-  </a-button>
+  <a-col :span="options.width" :offset="options.offset">
+    <a-button :disabled="options.attribute.disabled" :loading="options.attribute.loading"
+              :size="options.attribute.size" :type="options.attribute.type"
+              :html-type="options.attribute.htmlType"
+              :icon="options.attribute.icon" @click="onClick">
+      {{options.text}}
+    </a-button>
+  </a-col>
 </template>
 
 <script>
-  import {Button} from 'ant-design-vue'
+  import {Button, Col} from 'ant-design-vue'
   export default {
     name: "m-button",
     components : {
-      AButton: Button
+      AButton: Button,
+      ACol: Col
     },
     props: {
       options: Object
@@ -19,6 +23,7 @@
     methods : {
       onClick () {
         // console.log(this.options.attribute.inner)
+        (this.options.attribute && this.options.attribute.inner) &&
         this.$emit('submitEvent', this.options.attribute.inner)
       }
     }
